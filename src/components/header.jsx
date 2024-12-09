ec274130b35192eb671922015ae04a2d304c3c88
vstheven <victor.stheven@outlook.com>
2024-12-09 00:12:41 -0300

    feat(appbar): change app bar bg color

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,33 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import { Drawer,ListItem,ListItemButton,ListItemIcon,ListItemText } from '@mui/material'
-import TaskIcon from '@mui/icons-material/Task'
-import HomeIcon from '@mui/icons-material/Home'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import TaskIcon from "@mui/icons-material/Task";
+import HomeIcon from "@mui/icons-material/Home";
 
 function Header() {
-
-  const [open,setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = React.useCallback(() => {
-    setOpen(open => !open)
-  },[])
+    setOpen((open) => !open);
+  }, []);
 
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="sticky" >
-          <Toolbar
-            variant='dense'
-          >
+        <AppBar position="sticky" color="secondary">
+          <Toolbar variant="dense">
             <IconButton
               size="large"
               edge="start"
@@ -33,9 +36,7 @@ function Header() {
               sx={{ mr: 2 }}
               onClick={toggleDrawer}
             >
-
               <MenuIcon />
-
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Gerenciamento de Projetos
@@ -69,7 +70,7 @@ function Header() {
         </Box>
       </Drawer>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
