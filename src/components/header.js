50dbbcb4dc62ba8a20a692e14ada79cd0a80717c
vstheven <victor.stheven@outlook.com>
2024-12-09 00:09:49 -0300

    feat: add lateral menu

--- src/components/header.js
+++ src/components/header.js
@@ -5,30 +5,70 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
-//import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu'
+import { Drawer,ListItem,ListItemButton,ListItemIcon,ListItemText } from '@mui/material'
+import TaskIcon from '@mui/icons-material/Task'
+import HomeIcon from '@mui/icons-material/Home'
 
 function Header() {
+
+  const [open,setOpen] = React.useState(false)
+
+  const toggleDrawer = React.useCallback(() => {
+    setOpen(open => !open)
+  },[])
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="sticky">
+          <Toolbar
+            variant='dense'
           >
-            {/*  
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer}
+            >
+
               <MenuIcon />
-              */}
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Gerenciamento de Projetos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Gerenciamento de Projetos
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer open={open} onClose={() => toggleDrawer(false)}>
+        <Box
+          width={250}
+          role="presentation"
+          onClick={() => toggleDrawer(false)}
+          onKeyDown={() => toggleDrawer(false)}
+        >
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Home"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <TaskIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Tarefas"} />
+            </ListItemButton>
+          </ListItem>
+        </Box>
+      </Drawer>
+    </>
   )
 }
 
